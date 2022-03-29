87f301f6e9f9e393f484db0f53552919908b1407
Brad Irby <brad@bradirby.com>
2022-03-29 14:24:38 +0200

    Creating Explicit test to regenerate interaction model

---
+++ src/AlexaSampleSkill.Tests/ModelGenerationTests.cs
@@ -0,0 +1,30 @@
+using System;
+using System.IO;
+using System.Text.Json;
+using NUnit.Framework;
+
+namespace AlexaSampleSkill.Tests
+{
+    /// <summary>
+    /// These tests are all explicit and are used to generate the various supporting files.  Run
+    /// them when you want to regenerate the files
+    /// </summary>
+    public class ModelGenerationTests
+    {
+      
+
+        [Test]
+        [Explicit]
+        public void CreateInteractionModelFile()
+        {
+            var skill = new BasicSkill();
+            var interactionModelObj = skill.GetInteractionModel();
+            var interactionModelStr = JsonSerializer.Serialize(interactionModelObj);
+
+            var filePath = Environment.GetEnvironmentVariable("AlexaNetCoreRootFolder");
+            filePath = "D:\\Solutions\\Alexa\\AlexaNetCore\\AlexaNetCore\\src";
+            filePath = Path.Combine(filePath, "AlexaSampleSkill\\SupportingFiles\\InteractionModelEnglish.json");
+            File.WriteAllText(filePath, interactionModelStr);
+        }
+    }
+}
